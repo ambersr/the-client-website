c854290aaf4bc32d87b62012ba93eaf624a60676
ambersr <amber.schalker@hva.nl>
2024-10-02 15:40:43 +0200

    Navbar mobile first gemaakt + font size responsive gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,17 @@
 //     } 
 //   });
 // }
+
+
+
+
+// Code voor openklappen hamburger menu in nav
+
+const menuButton = document.querySelector("#menuButton");
+const hetMenu = document.querySelector("#menu");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    hetMenu.classList.toggle("open");
+}
